dc58248c6d51598e1c764166fd3551d12804c95c
Mattullon <mattullon@gmail.com>
2023-04-25 00:21:26 -0400

    inicio de curso

---
+++ citas-react/src/components/Paciente.jsx
@@ -0,0 +1,31 @@
+
+const Paciente = () => {
+  return (
+    <div>
+       
+        <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+            <p className="font-bold mb-3 text-gray-700 uppercase"> Nombre:{''}
+                <span className="font-normal normal-case"> Garu </span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase"> Propietario:{''}
+                <span className="font-normal normal-case"> Ana </span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase"> Email:{''}
+                <span className="font-normal normal-case"> correo@correo.com </span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase"> Alta:{''}
+                <span className="font-normal normal-case"> 10 de diciembre 2022 </span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase"> Sintomas:{''}
+                <span className="font-normal normal-case"> Lorem ipsum dolor sit,
+                 amet consectetur adipisicing elit. Delectus quo iure, aperiam 
+                  cupiditate eveniet eaque quos ab, 
+                 earum recusandae? Quidem accusantium nihil dolorem eaque qui ut 
+                 fuga ullam. </span>
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Paciente
